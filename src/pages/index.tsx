975d2454a291c76da87c0e0ce154e9af8dd3b4a6
Wan Saleh <wansaleh@gmail.com>
2021-12-29 18:59:43 +0800

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="container p-20 mx-auto">
+      <b>OG Generator</b>
+      <code className="block">
+        /api/og?... logo, siteName, description, theme, title, logoWidth,
+        logoHeight
+      </code>
+    </div>
+  );
+}
